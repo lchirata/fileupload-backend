cd771e63ca1d0bc6a17ea3f0a29c0988bf529627
lchirata <larissa.c.hirata@gmail.com>
2020-07-30 19:41:55 -0300

    multer - uploadFile

--- src/routers.js
+++ src/routers.js
@@ -1,8 +1,17 @@
 const routers = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer')
 
 routers.get('/', (req, res) => {
    // return res.send('Hello Word !')
-   return res.json({ hello: "test"})
+   return res.json({ hello: "word"})
 });
+
+routers.post('/posts', multer(multerConfig).single("file") ,(req, res) => {
+    // return res.send('Hello Word !')
+    console.log(req.file);
+    return res.json({ hello: "word"})
+ });
+
 
 module.exports = routers;
