788513459bef3e3d60e01324f7aa5036e6de9cfb
lchirata <larissa.c.hirata@gmail.com>
2020-07-31 17:36:15 -0300

    delete

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const aws = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+
+const s3 = new aws.S3();
 
 const PostSchema = new mongoose.Schema({
     name: String,
@@ -11,5 +16,33 @@ const PostSchema = new mongoose.Schema({
         default: Date.now,
     }
 });
+
+PostSchema.pre("save", function() {
+    if (!this.url) {
+      this.url = `${process.env.APP_URL}/files/${this.key}`;
+    }
+  });
+
+PostSchema.pre("remove", function() {
+    if (process.env.STORAGE_TYPE === "s3") {
+      return s3
+        .deleteObject({
+          Bucket: "uploadexampleserver",
+          Key: this.key
+        })
+        .promise()
+        .then(response => {
+          console.log(response.status);
+        })
+        .catch(response => {
+          console.log(response.status);
+        });
+    } else {
+      return promisify(fs.unlink)(
+        path.resolve(__dirname, "..", "..", "tmp", "uploads", this.key)
+      );
+    }
+  });
+  
 
 module.exports = mongoose.model("Post", PostSchema);
