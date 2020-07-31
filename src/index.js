dec1cf9440d939b7d17146a19bd3a06168d30fdf
lchirata <larissa.c.hirata@gmail.com>
2020-07-31 12:06:26 -0300

    configurations mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const morgan = require('morgan')
+const mongoose = require('mongoose');
+
 const app = express();
+
+/*
+Database Setup
+*/ 
+mongoose.connect('mongodb://localhost:27017/upload', 
+    {
+        userNewUrlParser: true
+    }
+);
+
 
 app.use(express.json()); //express consegue aceitar requisições em formato de json
 app.use(express.urlencoded({ extended: true})); // facilita na parte de envio de arquivos
