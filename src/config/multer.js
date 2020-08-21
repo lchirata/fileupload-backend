3f62bffc4c8ad4e9b9ee3d19a729c0e283b8bb58
lchirata <larissa.c.hirata@gmail.com>
2020-08-21 11:55:38 -0300

    aceitando todos os tipos de arquivos

--- src/config/multer.js
+++ src/config/multer.js
@@ -41,14 +41,15 @@ module.exports = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
   storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
-    fileSize: 2 * 1024 * 1024
+    fileSize:  100 * 1024 * 1024
   },
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
-      "image/jpeg",
-      "image/pjpeg",
-      "image/png",
-      "image/gif"
+      file.mimetype
+      // "image/jpeg",
+      // "image/pjpeg",
+      // "image/png",
+      // "image/gif",
     ];
 
     if (allowedMimes.includes(file.mimetype)) {
